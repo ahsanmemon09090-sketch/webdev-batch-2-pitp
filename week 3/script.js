fb2c6f0c16b9ef5fcaaedee6daf9c634e2ef550f
ahsan <ahsanmemon09090@gmail.com>
2025-12-08 17:27:00 +0500

    upload new file

---
+++ week 3/script.js
@@ -0,0 +1,17 @@
+const menubtn=document.getElementById(menu-toggle);
+const navmenu=document.querySelector(.nav-);
+
+
+let rollnumber = 0055
+
+//if// (rollnumber== 0055) {
+    // console.log("hello iam ahsan!")
+    //}else {
+        //  console.log ("i do not have a name ")
+        //}
+    
+    //let isloggeding = true
+    
+    const msg = rollnumber == 0055 ? "hello iam ahsan !" : " i do m not have  name"
+
+console.log(msg)    
